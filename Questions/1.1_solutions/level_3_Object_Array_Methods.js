2354e0a5dd9446299bc3327a427ab1b15667f15f
madesears <solankeadi11@gmail.com>
2026-06-06 22:11:37 +0530

    LEVEL 3: Challenge 3.2 - mistake corrected

--- Questions/1.1_solutions/level_3_Object_Array_Methods.js
+++ Questions/1.1_solutions/level_3_Object_Array_Methods.js
@@ -73,13 +73,18 @@ const library = {
     name: "Central Library",
     books: [
         { title: "The Hobbit", author: "J.R.R. Tolkien", isbn: "978-0261102217", borrowedBy: null, isAvailable: true },
+        { title: "Animal Farm", author: "George Orwell", isbn: "978-0451526342", borrowedBy: null, isAvailable: true }, // New
         { title: "1984", author: "George Orwell", isbn: "978-0451524935", borrowedBy: "Baka", isAvailable: false },
+        { title: "The Silmarillion", author: "J.R.R. Tolkien", isbn: "978-0345325815", borrowedBy: "Ciel", isAvailable: false }, // New
         { title: "To Kill a Mockingbird", author: "Harper Lee", isbn: "978-0446310789", borrowedBy: null, isAvailable: true },
+        { title: "The Illustrated Man", author: "Ray Bradbury", isbn: "978-1451678185", borrowedBy: null, isAvailable: true }, // New
         { title: "The Great Gatsby", author: "F. Scott Fitzgerald", isbn: "978-0743273565", borrowedBy: "Ciel", isAvailable: false },
         { title: "Brave New World", author: "Aldous Huxley", isbn: "978-0060850524", borrowedBy: null, isAvailable: true },
+        { title: "Dune Messiah", author: "Frank Herbert", isbn: "978-0441172696", borrowedBy: "Esmo", isAvailable: false }, // New
         { title: "Fahrenheit 451", author: "Ray Bradbury", isbn: "978-1451673319", borrowedBy: "Baka", isAvailable: false },
         { title: "The Catcher in the Rye", author: "J.D. Salinger", isbn: "978-0316769174", borrowedBy: null, isAvailable: true },
         { title: "Dune", author: "Frank Herbert", isbn: "978-0441172719", borrowedBy: "Baka", isAvailable: false },
+        { title: "The Fellowship of the Ring", author: "J.R.R. Tolkien", isbn: "978-0618346257", borrowedBy: null, isAvailable: true }, // New
         { title: "Neuromancer", author: "William Gibson", isbn: "978-0441569595", borrowedBy: null, isAvailable: true },
         { title: "Foundation", author: "Isaac Asimov", isbn: "978-0553293357", borrowedBy: "Ne Zha", isAvailable: false }
     ],
@@ -91,14 +96,28 @@ const library = {
         if(book.borrowedBy !== null) return `${book.title} is already borrowed, find another`;
         
         book.borrowedBy = borrowerName;
-        book.isAvailable = book.borrowedBy !== null ? false: true;
+        book.isAvailable = false;;
     
         return book;
+    },
+
+    getAllBooks(personName){
+        return this.books.filter( book => book.borrowedBy === personName)
+        .map( book => book.title );
+    },
+
+    getAvailableBooksByAuthor(authorName){
+        const authorbooks = this.books.filter( book => book.author === authorName && book.isAvailable === true);
+
+        return authorbooks.length > 0 ? authorbooks : `Not Found Any`;
     }
 
 
 };
 
 // Check the books array reference
-console.log(library.borrowBookByISBN("978-0441569595", "Kami"));
-console.log(library.books);
+// console.log(library.borrowBookByISBN("978-0441569595", "Kami"));
+// console.log(library.getAllBooks("Baka"));
+console.log(library.getAvailableBooksByAuthor("George Orwell"));
+
+// console.log(library.books);
