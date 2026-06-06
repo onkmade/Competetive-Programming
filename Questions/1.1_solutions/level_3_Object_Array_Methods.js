cbf3016ec8fd863e40e21ebc2945fbbae0ab2f05
madesears <solankeadi11@gmail.com>
2026-06-06 20:52:07 +0530

    LEVEL 3: Objects + Arrays + Methods: Challenge 3.1 Completed

--- Questions/1.1_solutions/level_3_Object_Array_Methods.js
+++ Questions/1.1_solutions/level_3_Object_Array_Methods.js
@@ -5,53 +5,62 @@ function lineS(){
     return `\n\n\n================= Challenge 3.${++num} =================\n`
 }
 
-console.log(lineS());
-
-
 // Challenge 3.1
+console.log(lineS());
 const school = {
-    name: "Baka Institute of Technology",
-    departments: [
-        {
-            deptName: "Computer Science & Engineering",
-            courses: [
-                {
-                    courseName: "Web Development",
-                    credits: 4,
-                    enrolledStudents: ["Luffy", "Zoro", "Nami"]
-                },
-                {
-                    courseName: "Data Structures",
-                    credits: 3,
-                    enrolledStudents: ["Zoro", "Robin"]
-                }
-            ]
-        },
-        {
-            deptName: "Electronics & Telecommunication",
-            courses: [
-                {
-                    courseName: "Microcontrollers",
-                    credits: 4,
-                    enrolledStudents: ["Chopper", "Franky"]
-                },
-                {
-                    courseName: "Signal Processing",
-                    credits: 3,
-                    enrolledStudents: ["Sanji", "Usopp", "Nami"]
-                }
-            ]
-        }
-    ]
-};
+    name: "Tech Institute", 
+    departments: [],
 
-const addDepartment = (deptName, courses, obj) => {
-    if(obj.departments){
-        departments[deptName] = deptName;
-        departments[courses] = {};
+    addDepartment(deptName){
+        const newDepartment = { name: deptName, courses: [] };
+        this.departments.push(newDepartment);
+    },
+
+    addCourseToDept(deptName, courseName, credits){
+        const department = this.departments.find( dept => dept.name === deptName );
+
+        if(department){
+            const newCourse = { courseName, credits, enrolledStudents: [] }
+            department.courses.push(newCourse);
+
+            return `${newCourse} has been added in the ${deptName}`
+        }
+
+        return `${deptName} not found`;
+    },
+
+    enrolledStudentsToCourse(deptName, courseName, studentName){
+        const department = this.departments.find( d => d.name === deptName );
+        if(!department) return `Invalid ${deptName}`
+        
+        const course = department.courses.find( c => c.courseName === courseName);
+        if(!course) return `Invalid ${courseName}`
+
+        return course.enrolledStudents.push(studentName);
+    },
+    
+    
+    getAllStudents(deptName){
+        const department = this.departments.find( dept => dept.name === deptName);
+        if(!department) return `Invalid ${deptName}`
+
+        return department.courses.flatMap(course => course.enrolledStudents);
     }
 
-    return obj;
 }
 
-console.log(addDepartment("Mechnical Engineering", { courseName: "Rigid Bodies", credits: 5, enrolledStudents: ["Baka", "Sonu", "Tapu"]}))
+school.addDepartment("Engg");
+school.addDepartment("Science");
+school.addCourseToDept("Engg", "Web", 4);
+school.addCourseToDept("Engg", "JS", 4);
+school.addCourseToDept("Science", "Go", 4);
+school.enrolledStudentsToCourse("Engg", "Web", "Baka");
+school.enrolledStudentsToCourse("Engg", "Web", "Nooxie");
+school.enrolledStudentsToCourse("Engg", "JS", "Ne Zha");
+school.enrolledStudentsToCourse("Engg", "JS", "Esmo");
+school.enrolledStudentsToCourse("Engg", "JS", "Ciel");
+school.enrolledStudentsToCourse("Science", "Go", "Kupe");
+
+console.log(school.departments[0].courses);
+
+console.log(school.getAllStudents("Engg"));
--- test.js
+++ test.js
@@ -1,10 +1,36 @@
-function addTimeStamp(user){
-    user.lastLoggined = new Date();
-    return user;
-}
+const hero = {
+    name: "Ne Zha",
+    health: 100,
+    level: 1,
 
-const user = { name: "Alice" };
-addTimeStamp(user);
+    // Hero is an entity which has another sub entity called weapon
+    // It's an entity with its own properties.
+    weapon: {
+        name: "Argon Thunder",
+        damage: 15,
+        durability: 100, 
+        type: "sword"
+    },
 
-console.log(user);
+    // Armor is an another entity
 
+    armor: {
+        name: "Iron-Chest",
+        defense: 20, 
+        durability: 100, 
+        type: "chest"
+    },
+
+    inventory: [
+        {name: "Healing Spell", type: "healer", effect: { heal: 50}},
+        {name: "Poison", type: "destroy", effect: { damage: 40}},
+        {name: "Golden Coin", currency: 100, quantity: 150}
+    ],
+};
+
+
+const item = [...hero.inventory];
+item.push( { name: "New Item" });
+
+console.log(item);
+console.log(hero.inventory);
