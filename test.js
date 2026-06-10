75266945d1b4cedfc02a1e49fd8fa36668709d27
madesears <solankeadi11@gmail.com>
2026-06-10 16:27:55 +0530

    Level 4: Static Keyword: How to toggle the sort using Copied array

--- test.js
+++ test.js
@@ -1,10 +1,1 @@
-class ClassWithStaticMethods{
-    Property = "Namaste";
-    static Property = "COOOL";
-
-    static {
-        console.log("Just the static keyword");
-    }
-}
-
-console.log(ClassWithStaticMethods);
+/*  */
